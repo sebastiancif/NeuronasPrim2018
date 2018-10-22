c8892045d4eb653ef6fd02c9c3f55250b53924cc
sebastiancif <sebastian.cifuentes@ing.uchile.cl>
2018-10-22 16:29:04 -0300

    actualizando el git

--- Clase 4/network.py
+++ Clase 4/network.py
@@ -1,30 +1,77 @@
 import layer as ly
+import random
 import numpy
-
+import re
+import os
 # float -> float
 # Derivative of sigmoid function
 def transferDerivative(x):
     return x*(1.0-x)
 
+# string -> File
+# Reordena las lineas de un archivo dado
+def sortFileRows(filename):
+    random.seed()
+    with open(filename,'r') as source:
+        next(source)
+        data = [(random.random(),line) for line in source]
+    source.close()
+    data.sort()
+    with open('random'+filename,'w') as output:
+        for _, line in data:
+            output.write(line)
+    output.close()
+    return "random"+filename
+
 class Network:
 
-    # int, int, int, [int ... int] -> Network
+    # int, int, int, List<int> -> Network
     # Receives number of layers, number of inputs and number of neurons per layers of the network
     def __init__(self, nlayers, ninputs, nneuronslayer):
         self.mylayers = []
         self.nlastlayer = nlayers-1
+        self.ninputs = ninputs
         previousneurons = ninputs
         for i in range(nlayers):
             self.mylayers.append(ly.Layer(nneuronslayer[i], previousneurons))
             previousneurons = nneuronslayer[i]
 
-    # [float ... float], [float ... float] -> None
+    # List<float> List<float> -> None
     # Trains the network using the given inputs and desiredoutputs
     def train(self, input, desiredoutput, learningrate = 0.2):
         lr = learningrate
         output = self.feed(input)
         self.errorBackpropagation(desiredoutput)
         self.updateWeightandBias(input, lr)
+
+    # string, string, [float] -> List<List<float>>
+    # Entrena a la red con una línea de string, separada por el indicador separator
+    def trainLine(self, line, separator, learningrate = 0.2):
+        myline = re.sub('\n', '', line)
+        wordlist = myline.split(separator)
+        self.train(wordlist[:self.ninputs],[self.ninputs:], learningrate)
+
+
+
+    def trainDataSet(self, datasetname, validationsetname, separator, nepoch, learningrate=0.2, unsortepoch = False):
+        input = datasetname
+        for epoch in nepoch:
+            if(unsortepoch):
+                input = sortFileRows(input)
+            with open(input,'r') as trainingdata:
+                for line in trainingdata:
+                    self.trainLine(line, separator, learningrate)
+
+
+
+        for epoch in range(nepoch):
+            for line in dataset:
+                train(line)
+        epoch
+        precision
+        errorvector
+        if(unsortepoch):
+            os.remove(input)
 
     # [float ... float] -> [float ... float]
     # Receives a Python list as input, returns the valuation of the network
--- Clase 4/xorlearning.py
+++ Clase 4/xorlearning.py
@@ -19,21 +19,25 @@ class XORNetwork:
     # Reinicia la red neuronal XOR
     def reset(self):
         self.network = nw.Network(2,2,[3,1])
+        print("Red reseteada!")
+
+    def feed(self, input):
+        return self.network.feed(input)
 
     # int -> None
     # Entrena a la red XOR con ejemplos de XOR aleatorios según la cantidad especificada
-    def trainNTimes(self, n):
+    def trainNTimes(self, n, learningrate = 0.2):
         numpy.random.seed()
         trainx = numpy.random.randint(0,2,n)
         trainy = numpy.random.randint(0,2,n)
         for i in range(len(trainx)):
-            self.network.train([trainx[i],trainy[i]],[operator.xor(trainx[i],trainy[i])])
-        print("Red entrenada"+str(n)+" veces!")
+            self.network.train([trainx[i],trainy[i]],[operator.xor(trainx[i],trainy[i])], learningrate)
+        print("Red entrenada "+str(n)+" veces!")
 
     # int, int, int -> [float ... float]
     # Recibe numero de entrenamientos, paso de evaluacion y cantidad de casos de test
     # Retorna la cantidad de aciertos por paso en un arreglo
-    def getPrecisionOfTraining(ntrains, trainstep, ntest):
+    def getErrorOfTraining(self, ntrains, trainstep, ntest):
         numpy.random.seed()
         testx = numpy.random.randint(0,2,ntest)
         testy = numpy.random.randint(0,2,ntest)
